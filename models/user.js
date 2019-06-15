96034d5234e1c345f5ebd280d0ea08867dd042de
vanphuong479@gmail.com <0989744720p>
2019-06-16 05:45:05 +0700

    h

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    firstname:{
+        type: String,
+        required: true
+    },
+    lastname:{
+        type: String,
+        required: true
+    },
+    accountType:{
+        type: String, // 1 driver, 2: passenger
+        required: true,
+        // default: 2
+    },
+    phone:{
+      type: String,
+      required: true
+    },
+    isActive:{
+      type: Boolean,
+      required: true
+    }
+})
+
+module.exports = user = mongoose.model('user',UserSchema);
