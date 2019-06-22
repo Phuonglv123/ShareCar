622bf5fcd8ccf77d36aa5ada82c7ae5cf45214a0
vanphuong479@gmail.com <0989744720p>
2019-06-23 00:56:04 +0700

    h

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
     res.set({
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-        'Access-Control-Allow-Headers': 'Authorization',
+        'Access-Control-Allow-Headers': "Content-Type, Authorization",
     });
     next();
 })
